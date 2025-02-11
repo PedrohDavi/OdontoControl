b9166cd1ba8f5890ee80654108e066b9f9941647
Pedro Davi <pedro.henrique04896@gmail.com>
2025-02-11 12:07:48 -0300

    add filtro de pesquisa

--- frontend/src/pages/ListaProdutos.tsx
+++ frontend/src/pages/ListaProdutos.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import React from "react";
 import { CardProduto } from "../components/CardProduto/index.tsx";
+import { Navbar } from "../components/Navbar/index.tsx"; 
 
 interface Produto {
     id: number;
@@ -14,37 +15,50 @@ interface Produto {
     foto?: string;
 }
 
-export function ListaProdutos(){
+export function ListaProdutos() {
     const [produtos, setProdutos] = useState<Produto[]>([]);
+    const [filtroNomeProduto, setFiltroNomeProduto] = useState(""); 
 
     useEffect(() => {
         fetchProdutos();
     }, []);
 
-    
-
-    const fetchProdutos = async () =>{
+    const fetchProdutos = async () => {
         try {
-            const req = await axios.get('http://localhost:5000/getProduct')
-        
-            const produtos = req.data;
-            setProdutos(produtos);
+            const req = await axios.get('http://localhost:5000/getProduct');
+            setProdutos(req.data);
         } catch (error) {
             console.log("Erro ao buscar Materiais");
-            
         }
-    }
+    };
 
-    const atualizarQuantidade = (id, novaQuantidade) => {
+    const atualizarQuantidade = (id: number, novaQuantidade: number) => {
         setProdutos((prev) =>
             prev.map((produto) => (produto.id === id ? { ...produto, quantidade: novaQuantidade } : produto))
         );
     };
-    return(
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-20">
-      {produtos.map((produto, index) => (
-        <CardProduto key={index} produto={produto} onUpdate={atualizarQuantidade}/>
-      ))}
-    </div>
-    )
+
+
+    return (
+        <>
+            {/* Navbar com pesquisa dinâmica */}
+            <Navbar setFiltroNomeProduto={setFiltroNomeProduto} />
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-20 p-4">
+                {produtos.length > 0 ? (
+                    produtos
+                        .filter((produto) =>
+                            produto.nome.toLowerCase().includes(filtroNomeProduto.toLowerCase())
+                        )
+                        .map((produto, index) => (
+                            <CardProduto key={index} produto={produto} onUpdate={atualizarQuantidade} />
+                        ))
+                ) : (
+                    <div className="flex justify-center items-center h-[60vh] w-full col-span-full">
+                        <p className="text-2xl font-semibold text-gray-500">Nenhum material encontrado</p>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 }
